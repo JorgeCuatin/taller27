ef6c65dd21ab9acc730ce19b9d1d0e808e6058ab
JorgeCuatin <jorgecuatin2012@gmail.com>
2020-07-24 19:21:57 -0500

    taller_27_nodos2

--- js/javascript.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-var a1 =document.getElementsByTagName("a")
-console.log(a1);
-console.log("tamaño: " +a1.length);
-console.log("Penultimo elemento; "+ a1[a1.length-1])
-}
